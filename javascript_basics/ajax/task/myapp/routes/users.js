3af164c82e0e8af4826088b4f2716be69683cd96
Mate Balo Bordas <mate.balob@gmail.com>
2015-11-09 15:25:44 +0100

    updated myapp node server

--- javascript_basics/ajax/task/myapp/routes/users.js
+++ javascript_basics/ajax/task/myapp/routes/users.js
@@ -3,33 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    function jsonCallback() {
-        return {
-            users:
-            [
-                {
-                    userName: 'user 1'
-                },
-                {
-                    userName: 'user 2'
-                },
-                {
-                    userName: 'user 3'
-                }
-            ]
-        }
-    }
 
-    //res.send(jsonCallback());
     res.jsonp([
         {
-            userName: 'user 1'
+            username: 'user 1',
+            beaconCount: 1,
+            elapsedMinutes: 5
         },
         {
-            userName: 'user 2'
+            username: 'user 2',
+            beaconCount: 2,
+            elapsedMinutes: 10
         },
         {
-            userName: 'user 3'
+            username: 'user 3',
+            beaconCount: 3,
+            elapsedMinutes: 15
         }
     ]);
 
