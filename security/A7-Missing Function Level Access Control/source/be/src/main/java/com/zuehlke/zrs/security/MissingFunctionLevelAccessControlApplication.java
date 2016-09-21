67ae820a7cf2163f55c8fe8b6bccd0a98c754db0
Nenad Spuzic <nenad.spuzic@zuhlke.com>
2016-09-21 14:18:20 +0200

    Added sample BE project

---
+++ security/A7-Missing Function Level Access Control/source/be/src/main/java/com/zuehlke/zrs/security/MissingFunctionLevelAccessControlApplication.java
@@ -0,0 +1,12 @@
+package com.zuehlke.zrs.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingFunctionLevelAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingFunctionLevelAccessControlApplication.class, args);
+	}
+}
