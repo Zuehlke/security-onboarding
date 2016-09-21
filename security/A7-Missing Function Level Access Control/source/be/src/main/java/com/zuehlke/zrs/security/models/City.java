0875ff466265d18f61712437c7dd4c7530d23f1d
Nenad Spuzic <nenad.spuzic@zuhlke.com>
2016-09-21 14:52:52 +0200

    Added city model and repository

---
+++ security/A7-Missing Function Level Access Control/source/be/src/main/java/com/zuehlke/zrs/security/models/City.java
@@ -0,0 +1,45 @@
+package com.zuehlke.zrs.security.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by nesp on 21-Sep-16.
+ */
+@Entity
+public class City implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String state;
+
+    // ... additional members, often include @OneToMany mappings
+
+    protected City() {
+        // no-args constructor required by JPA spec
+        // this one is protected since it shouldn't be used directly
+    }
+
+    public City(String name, String state) {
+        this.name = name;
+        this.state = state;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+}
---
+++ security/A7-Missing Function Level Access Control/source/be/src/main/java/com/zuehlke/zrs/security/repositories/CityRepository.java
@@ -0,0 +1,14 @@
+package com.zuehlke.zrs.security.repositories;
+
+import com.zuehlke.zrs.security.models.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+public interface CityRepository extends Repository<City, Long> {
+
+    Page<City> findAll(Pageable pageable);
+
+    City findByNameAndStateAllIgnoringCase(String name, String state);
+
+}
