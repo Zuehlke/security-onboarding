0779717dc573b8e2ea29fada524ea9fefeaa6990
Nenad Spuzic <nenad.spuzic@zuhlke.com>
2016-09-22 13:21:09 +0200

    security/a7-missing-function-level-access-control: Cors headers setup

--- security/A7-Missing Function Level Access Control/source/be/src/main/java/com/zuehlke/zrs/security/MissingFunctionLevelAccessControlApplication.java
+++ security/A7-Missing Function Level Access Control/source/be/src/main/java/com/zuehlke/zrs/security/MissingFunctionLevelAccessControlApplication.java
@@ -3,13 +3,27 @@ package com.zuehlke.zrs.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MissingFunctionLevelAccessControlApplication {
 
-	public static void main(String[] args) {
-		final ConfigurableApplicationContext ctx = SpringApplication.run(MissingFunctionLevelAccessControlApplication.class, args);
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext ctx = SpringApplication.run(MissingFunctionLevelAccessControlApplication.class, args);
 
-		ctx.getBean(Bootstrapper.class).onStartup();
-	}
+        ctx.getBean(Bootstrapper.class).onStartup();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
