7491bf9736f6c5e81c34ebc10a9f240ef52a8977
Nenad Spuzic <nenad.spuzic@zuhlke.com>
2016-09-28 11:05:44 +0200

    Login form styled displaying errors

--- security/A7-Missing Function Level Access Control/source/fe/app/login/login.component.ts
+++ security/A7-Missing Function Level Access Control/source/fe/app/login/login.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute, Router, NavigationEnd, Event, NavigationStart, RoutesRecognized} from "@angular/router";
 
 @Component({
   selector: 'login',
   templateUrl: './login.html',
-
   moduleId: module.id,
+  styleUrls: ['./login.css']
 })
 export class LoginComponent {
 
   user: {name: string, password: string} = {name: '', password: ''};
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
-
-  ngOnInit() {
-  }
 }
