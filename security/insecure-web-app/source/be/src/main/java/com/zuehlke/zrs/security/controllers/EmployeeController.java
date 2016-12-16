d6537544f021073b9b5f02529974abb09094c049
Nenad Spuzic <nenad.spuzic@zuhlke.com>
2016-12-16 15:07:18 +0100

    Security: Fixed after merge build error

--- security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
+++ security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
@@ -5,20 +5,15 @@ import com.zuehlke.zrs.security.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
@@ -67,5 +62,4 @@ public class EmployeeController {
     void delete(long id) {
         employeeRepository.delete(id);
     }
-}
 }
