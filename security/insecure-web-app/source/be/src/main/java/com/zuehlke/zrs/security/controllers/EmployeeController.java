118ba1bca3746bf3ece55c7401fc0695bc12119f
Milan Starcevic <milan.starcevic@zuehlke.com>
2017-01-27 13:43:07 +0100

    Merge branch 'security/integrated' of https://bitbucket.org/ZuehlkeGlobal/zrs_curriculum into HEAD
    # Conflicts:
    #	security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java

--- security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
+++ security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
@@ -4,10 +4,19 @@ import com.zuehlke.zrs.security.models.Employee;
 import com.zuehlke.zrs.security.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.HttpStatus;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by nesp on 21-Sep-16.
@@ -18,22 +27,37 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeController {
 
     private EmployeeRepository employeeRepository;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public EmployeeController(EmployeeRepository employeeRepository) {
+    public EmployeeController(EmployeeRepository employeeRepository, JdbcTemplate jdbcTemplate) {
         this.employeeRepository = employeeRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     @Secured("USER")
     Iterable<Employee> index() {
-        return  employeeRepository.findAll();
+        return StreamSupport.stream(employeeRepository.findAll().spliterator(), false)
+                .filter(emp -> !emp.getTitle().equals("ADMIN"))
+                .collect(toList());
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    @Secured("USER")
+    Employee findById(@PathVariable String id) {
+        String sql = "SELECT * FROM EMPLOYEE WHERE ID = " + id;
+        try {
+            return (Employee) jdbcTemplate.queryForObject(sql,
+                    (RowMapper) (rs, num) -> new Employee(rs.getLong("ID"), rs.getString("FIRSTNAME"), rs.getString("LASTNAME"), rs.getString("TITLE")));
+        } catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    //@ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @ResponseBody
     void delete(@PathVariable long id) {
         employeeRepository.delete(id);
