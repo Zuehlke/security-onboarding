f3b4e306a9b2304211926d5c7cbb36032d029cce
Stefan Rankovic <stra@zuehlke.com>
2017-12-08 15:58:58 +0100

    Added missing import

--- security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
+++ security/insecure-web-app/source/be/src/main/java/com/zuehlke/zrs/security/controllers/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
