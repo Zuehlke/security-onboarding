7a2b975a4935e6cd076fde5ef1d1f8c3fe75e045
msto <marko.stojic@zuehlke.com>
2018-08-30 12:29:49 +0200

    Updated presentation with info about WebDriverManager library. Updated scripts to use it.

--- testing/04_Selenium/zLibrary_SeleniumTests/src/main/java/zLibrary/settings/BrowserFactory.java
+++ testing/04_Selenium/zLibrary_SeleniumTests/src/main/java/zLibrary/settings/BrowserFactory.java
@@ -1,5 +1,6 @@
 package zLibrary.settings;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,13 +15,16 @@ public class BrowserFactory implements PageConstants {
 
     public static WebDriver startBrowser(String browserName, String URL) {
         if(browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", DRIVER_PATH_FIREFOX_64);
+            //System.setProperty("webdriver.gecko.driver", DRIVER_PATH_FIREFOX_64);
+            WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         } else if(browserName.equalsIgnoreCase("ie")) {
-            System.setProperty("webdriver.ie.driver", DRIVER_PATH_IE);
+            //System.setProperty("webdriver.ie.driver", DRIVER_PATH_IE);
+            WebDriverManager.iedriver().setup();
             driver = new InternetExplorerDriver();
         } else {
-            System.setProperty("webdriver.chrome.driver", DRIVER_PATH_CHROME);
+            //System.setProperty("webdriver.chrome.driver", DRIVER_PATH_CHROME);
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
